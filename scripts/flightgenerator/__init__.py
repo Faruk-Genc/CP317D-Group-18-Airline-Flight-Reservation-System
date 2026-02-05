c3f036a628490aa2c76f95f6b7c9185b4e25f425
Oscar Saul <oscarsaul.mmv@gmail.com>
2026-02-04 21:53:47 -0500

    Update __init__.py

--- scripts/flightgenerator/__init__.py
+++ scripts/flightgenerator/__init__.py
@@ -1,3 +1,4 @@
 # backend/app/flightgenerator/__init__.py
 
-from .flight_data import generate_flights_for_date
+from .flight_data import generate_flights_for_date_json
+from .flight_data import generate_flights_for_date_SQLite
