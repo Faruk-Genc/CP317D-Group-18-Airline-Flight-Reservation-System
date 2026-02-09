9d11eaaf07d2cbae11cfcd15bf138117349e2e2e
Oscar Saul <oscarsaul.mmv@gmail.com>
2026-02-08 22:16:32 -0500

    Setup Neon serverless
    https://console.neon.tech/app/projects/wandering-resonance-86050727

--- backend/flight_queries.py
+++ backend/flight_queries.py
@@ -5,16 +5,15 @@ from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 
-DB_CONFIG = {
-    "dbname": os.getenv("DB_NAME"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT"),
-}
-
 def get_connection():
-    return psycopg2.connect(**DB_CONFIG, cursor_factory=RealDictCursor)
+    return psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        sslmode=os.getenv("DB_SSLMODE", "require")  # fallback to require
+    )
 
 def get_flights_on_date(date_str):
     """
