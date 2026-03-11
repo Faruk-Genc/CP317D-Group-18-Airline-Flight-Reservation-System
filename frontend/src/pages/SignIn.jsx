ec7eacb0f3d59aad75f6f905e7c5385135e4da5a
Ryan Tolentino <rgelo_tolentino@yahoo.ca>
2026-03-11 04:02:53 -0400

    Updated SignIn page and added styling

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import losAngeles from "../assets/featured/losangeles.jpg";
+import "./SignIn.css";
 
-export default function SignIn() {
+export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function handleSubmit() {
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log("Confirm Password:", confirmPassword);
+
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+  }
+
   return (
     <div
-      style={{
-        height: "calc(90vw)" 
-      }}
+      className="signup-page"
+      style={{ backgroundImage: `url(${losAngeles})` }}
     >
-      <h1 style={{color: "black"}}>Sign In</h1>
+      <div className="main-box-container">
+        <p className="sign-up-text">SIGN UP</p>
+
+        <input
+          placeholder="User name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button className="sign-up-button" onClick={handleSubmit}>
+          Sign Up
+        </button>
+
+        <p className="log-in-text">
+          Already have an account? <span className="log-in-here">Log in here</span>
+        </p>
+      </div>
     </div>
   );
 }
